13267e87aa4c17da1e6f15182910b6d0d7242a10
Philip Kheav <philip.kheav@hotmail.com>
2022-07-25 23:59:16 +1000

    feat(item): add initial item model

---
+++ item.rb
@@ -0,0 +1,12 @@
+require 'date'
+
+class Item
+  def initializer(name, quantity, unit, use_by_date)
+    @name = name
+    # use .to_i to allow both string and integer values to be passed
+    @quantity = quantity.to_i
+    @unit = unit
+    # Use Date.parse to allow more date formats to be be passed
+    @date = Date.parse(use_by_date)
+  end
+end
