cbc6f93fae0afdd1725d290945bed013a99fe516
Philip Kheav <philip.kheav@hotmail.com>
2022-07-26 18:59:43 +1000

    fix(whats_cooking_spec): use correct value

--- spec/whats_cooking_spec.rb
+++ spec/whats_cooking_spec.rb
@@ -3,6 +3,6 @@ require './lib/whats_cooking'
 describe WhatsCooking do
   it "should return the best recipes to cook based on items in the fridge csv list and their use by date" do
     whats_cooking = WhatsCooking.new('my-fridge.csv', 'my-recipes.json')
-    expect(whats_cooking.recommend).to eq("Vegemite")
+    expect(whats_cooking.recommend).to eq("Vegemite Sandwich")
   end
 end
