1034a401c7411758ac3343a0d06253a025487517
Philip Kheav <philip.kheav@hotmail.com>
2022-07-26 18:57:56 +1000

    chore(main): minor syntax updates and debugging

--- main.rb
+++ main.rb
@@ -1,15 +1,17 @@
 require './lib/whats_cooking'
 
 if ARGV.length != 2
-  puts "Error: please pass the fridge csv list file as the first argument"
-  puts "and the json recipe data file as the second argument"
-  puts "See below for example usage"
-  puts "ruby main.rb my-fridge.csv my-recipes.json"
+  puts 'Error: please pass the fridge csv list file as the first argument'
+  puts 'and the json recipe data file as the second argument'
+  puts 'See below for example usage'
+  puts 'ruby main.rb my-fridge.csv my-recipes.json'
   exit
 end
 
 # * is the splat operator and allows an array to be unpacked
 # so it can be passed as args to a function
 whats_cooking = WhatsCooking.new(*ARGV)
+
 puts whats_cooking.items.inspect
 puts whats_cooking.recipes
+puts whats_cooking.recommendations
