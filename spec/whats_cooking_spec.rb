9b44633eee6aeb07bf3e677655d9216edb1feb28
Philip Kheav <philip.kheav@hotmail.com>
2022-07-27 16:21:04 +1000

    feat(whats_cooking_spec): add takeout test

--- spec/whats_cooking_spec.rb
+++ spec/whats_cooking_spec.rb
@@ -1,8 +1,17 @@
 require './lib/whats_cooking'
 
 describe WhatsCooking do
-  it "should return the best recipes to cook based on items in the fridge csv list and their use by date" do
-    whats_cooking = WhatsCooking.new('my-fridge.csv', 'my-recipes.json')
-    expect(whats_cooking.recommend).to eq("Vegemite Sandwich")
+  context "when we don't have enough cheese in the fridge but enough bread and vegemite" do
+    it "should return Vegemite Sandwich as the recommended recipe" do
+      whats_cooking = WhatsCooking.new('my-fridge.csv', 'my-recipes.json')
+      expect(whats_cooking.recommend).to eq('Vegemite Sandwich')
+    end
+  end
+
+  context "when we don't have enough bread in the fridge for either sandwich recipes" do
+    it "should suggest getting takeout" do
+      whats_cooking = WhatsCooking.new('my-fridge2.csv', 'my-recipes.json')
+      expect(whats_cooking.recommend).to eq('Call for takeout')
+    end
   end
 end
